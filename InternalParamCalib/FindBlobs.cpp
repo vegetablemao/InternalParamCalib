173078fdee529e4b02d0ff5946ceb36290126763
vegetablemao <vegetablemao@gmail.com>
2016-04-18 21:04:35 +0800

    compute the coses

--- InternalParamCalib/FindBlobs.cpp
+++ InternalParamCalib/FindBlobs.cpp
@@ -1,5 +1,4 @@
 #include "FindBlobs.h"
-#include <iostream>
 #include <algorithm>
 
 namespace cv
@@ -81,7 +80,7 @@ namespace cv
 		{
 			for (int i = col+1; i < object.cols; i++) 
 			{ 				
-				object.col(i-1) = object.col(i); 	
+				object.col(i).copyTo(object.col(i-1)); 	
 			} 			
 			object = object.t();
 			object.pop_back();
@@ -105,11 +104,71 @@ namespace cv
 			Scalar s = sum(ipMat.col(i));
 			dist.push_back(s[0]);
 		}
-		std::vector<double>::iterator minIter = std::min_element(std::begin(dist), std::end(dist));
-		int mind = std::distance(std::begin(dist), minIter);
+		std::vector<double>::iterator minIter = std::min_element(dist.begin(), dist.end());
+		int mind = std::distance(dist.begin(), minIter);
 
 		return mind;
 
+	}
+
+	void findBlobs::sortTheDistFromThePoint(const Point& ip, const Mat& mc, vector<int>& idx)
+	{
+		idx.clear();
+		int numOfCol = mc.cols;
+		Mat mip = Mat(ip);
+		mip.convertTo(mip, CV_64FC1);
+		Mat ipMat = mip * Mat::ones(1,numOfCol, mip.type());
+		subtract(ipMat, mc, ipMat);
+		pow(ipMat, 2, ipMat);
+
+		std::vector<double> dist;
+		dist.clear();
+		for (int i = 0; i < numOfCol; i++)
+		{
+			Scalar s = sum(ipMat.col(i));
+			dist.push_back(s[0]);
+			idx.push_back(i);
+		}
+		sort(idx.begin(), idx.end(),
+			[& dist](size_t i1, size_t i2) {return dist[i1] <  dist[i2];});
+
+	}
+
+	void findBlobs::computeCos(const Mat& co, const Mat& cco, const Mat& cxy, 
+		const std::vector<Point>& pairs,
+		std::vector<double>& cosa, std::vector<double>& cosb)
+	{
+		Mat vxy = cxy - co * Mat::ones(1, cxy.cols, co.type());
+		Mat v =  co - cco;
+
+		
+		std::cout << vxy << std::endl;
+		std::cout << v << std::endl;
+		std::cout << co << std::endl;
+		std::cout << cco << std::endl;
+		std::cout << cxy << std::endl;
+
+		vector<double> vxyabs;
+		vxyabs.clear();
+		for (int i = 0; i < cxy.cols; i++)
+		{
+			double tt = norm(vxy.col(i));
+			vxyabs.push_back(norm(vxy.col(i)));
+		}
+		cosa.clear();
+		cosb.clear();
+		for (int i = 0; i < cxy.cols; i++)
+		{
+			int k1 = pairs[i].x;
+			int k2 = pairs[i].y;
+			Mat tmpa = vxy.col(k1).t() * vxy.col(k2);
+			double tcos = tmpa.at<double>(0,0) / ( vxyabs[k1] * vxyabs[k2] );
+			cosa.push_back(tcos);
+
+			Mat tmpb = v.t() * vxy.col(i);
+			tcos =  tmpb.at<double>(0, 0) / ( vxyabs[i] * norm(v.col(0)) );
+			cosb.push_back(tcos);
+		}
 	}
 
 	const Mat& findBlobs::findCentroidGrid()
@@ -124,26 +183,69 @@ namespace cv
 
 		//find the left top three points in the centroids.
 		int mind = findNearestPointInMatrix(origin, mc);
-		Mat cco = mc.col(mind);//orgin point
+		std::cout << mc << std::endl;
+		Mat cco(2, 1, mc.type());
+		mc.col(mind).copyTo(cco);	//orgin point
+
+		std::cout << cco << std::endl;
 		DeleteOneColOfMat(mc, mind);
+		std::cout << mc << std::endl;
+		std::cout << cco << std::endl;
 		m_mCentroidGrid.at<Vec2d>(0, 0) = cco;
 
 
 		mind = findNearestPointInMatrix(x1, mc);
-		Mat ccx = mc.col(mind);
+		std::cout << mc << std::endl;
+		Mat ccx(2, 1, mc.type());
+		mc.col(mind).copyTo(ccx);
+		std::cout << ccx << std::endl;
 		DeleteOneColOfMat(mc, mind);
+		std::cout << mc << std::endl;
 		m_mCentroidGrid.at<Vec2d>(0, 1) = ccx;
 
 
 		mind = findNearestPointInMatrix(y1, mc);
-		Mat ccy = mc.col(mind);
+		Mat ccy(2, 1, mc.type());
+		mc.col(mind).copyTo(ccy);
 		DeleteOneColOfMat(mc, mind);
 		m_mCentroidGrid.at<Vec2d>(1, 0) = ccy;
 
 		//find the first line of the centroids
+		std::vector<int> idx;
+		sortTheDistFromThePoint(x1, mc, idx);
+
+		const int tmpCountMax = 6;
+		int tmpidx[tmpCountMax] = {3, 4, 5, 6, 7, 8};
+		int tmpCount = 0;
+
+		Mat cxy;
+		cxy.create(2, 3, mc.type());
+		cxy = 0;
+		for(size_t i = 0; i < 3; i++)
+		{
+			mc.col(idx[i]).copyTo(cxy.col(i));
+			//cxy.col(i) = mc.col(idx[i]);
+		}
+		std::cout << mc << std::endl;
+		std::cout << cxy << std::endl;
+
+
+		std::vector<Point> pairs;
+		pairs.clear();
+		pairs.push_back(Point(1, 2));
+		pairs.push_back(Point(0, 2));
+		pairs.push_back(Point(0, 1));
+
+		std::vector<double> cosa;
+		std::vector<double> cosb;
+		computeCos(ccx, cco, cxy, pairs, cosa, cosb);
 
 
 
+
+
+		std::for_each (idx.begin(), idx.end(), printFunc);
+		std::cout << std::endl;
 		std::cout << mc.cols << std::endl;
 		std::cout << m_mCentroidGrid.at<Vec2d>(0, 0) << " " <<  m_mCentroidGrid.at<Vec2d>(0, 1) << " " <<  m_mCentroidGrid.at<Vec2d>(1, 0) << std::endl;
 		return m_mCentroidGrid;
--- InternalParamCalib/FindBlobs.h
+++ InternalParamCalib/FindBlobs.h
@@ -3,6 +3,7 @@
 #ifndef FINDBLOBS_H
 #define FINDBLOBS_H
 
+#include <iostream>
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
@@ -26,9 +27,12 @@ namespace cv
 		const Mat& findCentroidGrid();
 		
 	private:
-		static bool greaterMark(const contourType& s1, const contourType& s2) { return s1.size() > s2.size() ;};
+		static bool greaterMark(const contourType& s1, const contourType& s2) { return s1.size() > s2.size() ;}
+		static void printFunc(int i) { std::cout << ' ' << i; }
 		void DeleteOneColOfMat(Mat& object,int col);
 		int findNearestPointInMatrix(const Point& ip, const Mat& mc);
+		void computeCos(const Mat& co, const Mat& cco, const Mat& cxy, const std::vector<Point>& pairs, std::vector<double>& cosa, std::vector<double>& cosb);
+		void sortTheDistFromThePoint(const Point& ip, const Mat& mc, std::vector<int>& idx);
 	private:
 		contourContainer m_vContours;
 		centroidContainer m_vCentroids;
