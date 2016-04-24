7a933fada111161785fb16e33992dd042193761a
vegetablemao <vegetablemao@gmail.com>
2016-04-25 01:23:49 +0800

    4/25

---
+++ InternalParamCalib/calibrate.cpp
@@ -0,0 +1,39 @@
+#include "calibrate.h"
+
+namespace cv
+{
+	calibrate::calibrate()
+	{
+		m_sysCfg.blobgapx = 0;
+		m_sysCfg.blobgapy = 0;
+		m_sysCfg.blobradius = 0;
+		m_sysCfg.cata = 0;
+		m_sysCfg.focal = 0;
+		m_sysCfg.model.clear();
+		m_sysCfg.projtype.clear();
+		m_sysCfg.viewfield = 0;
+		m_sysCfg.cp.m_u = 0;
+		m_sysCfg.cp.m_v = 0;
+		m_sysCfg.cp.u0 = 0;
+		m_sysCfg.cp.v0 = 0;
+	}
+
+	calibrate::~calibrate()
+	{
+
+	}
+
+	void calibrate::readCentroidsGrid(const FileStorage& centGridFS)
+	{
+		Mat centroidsGrid;
+		centGridFS["centroidsGridMat"] >> centroidsGrid;
+
+	}
+
+	void calibrate::planecoords()
+	{
+
+	}
+
+
+}
--- InternalParamCalib/calibrate.h
+++ InternalParamCalib/calibrate.h
@@ -4,6 +4,10 @@
 #define CALIBRATE_H
 
 #include <string>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+
 
 namespace cv
 {
@@ -34,8 +38,14 @@ namespace cv
 
 		calibrate();
 		virtual ~calibrate();
-	private:
+		void readCentroidsGrid(const FileStorage& centGridFS);
 
+	private:
+		void planecoords();
+
+	private:
+		sysConfig m_sysCfg;
+		Mat m_mCoordMat;
 	};
 }
 
